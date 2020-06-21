3df44f7ff10d1579bac0f46bd9243ba321e35473
rburkard <roman.burkard@yahoo.de>
2020-06-21 20:58:03 +0200

    ready for heroku

---
+++ client/my-custom.js
@@ -0,0 +1,18 @@
+function storedata() {
+    const userEmail = document.getElementById("userInputEmail").value;
+    
+    var checkedBoxes = [];
+    $("input[name=custombox]:checked").each(function() {checkedBoxes.push(this.value)});
+
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({userEmail, checkedBoxes}),
+  };
+
+  console.log("test5:", options.body);
+
+  fetch("http://localhost:3000/api/add", options);
+}
